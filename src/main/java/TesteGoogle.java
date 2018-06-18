73c83af704f84658cbe8de3a7028de98c9e6fb5f
Diego Felipe <diegofelipe.14@gmail.com>
2018-06-18 08:32:48 -0400

    update

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -13,10 +13,10 @@ public class TesteGoogle {
 	public void testLoginElysium() throws InterruptedException {
 //		WebDriver driver = new 	FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://elysium.unimedfama.com.br/index.php/site/login");
+		driver.get("asdasdsa.com");
 		Assert.assertEquals("Login",driver.getTitle());
-		driver.findElement(By.id("user_login")).sendKeys("master");
-		driver.findElement(By.id("user_pass")).sendKeys("master");
+		driver.findElement(By.id("user_login")).sendKeys("asdsad");
+		driver.findElement(By.id("user_pass")).sendKeys("masasdasdter");
 		driver.findElement(By.xpath("//*[@id=\'login-form\']/input[4]")).click();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		Assert.assertEquals("Master",driver.findElement(By.xpath("/html/body/aside/div/section/div/div[2]/p")).getText());
